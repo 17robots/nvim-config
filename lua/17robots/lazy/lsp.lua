9016649b7e972b6ab56cbb3b82c32120174abae9
17robots <mdray@ameritech.net>
2024-08-23 17:49:05 -0400

    fix: no lsp

--- lua/17robots/lazy/lsp.lua
+++ lua/17robots/lazy/lsp.lua
@@ -19,11 +19,7 @@ return {
 
     require 'mason'.setup()
     require 'mason-lspconfig'.setup {
-      ensure_installed = {
-        'lua_ls',
-        'rust_analyzer',
-        'zls',
-      },
+      ensure_installed = {},
       handlers = {
         function(server_name)
           require 'lspconfig'[server_name].setup { capabilities = capabilities }
--- lua/17robots/lazy_init.lua
+++ lua/17robots/lazy_init.lua
@@ -13,5 +13,8 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   spec = "17robots.lazy",
-  change_detection = { notify = false }
+  change_detection = { notify = false },
+  rocks = {
+    enabled = false,
+  },
 })
