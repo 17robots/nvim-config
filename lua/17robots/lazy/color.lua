f8988cf065b98a1116eaa5c2b32a7c750213dfb4
17robots <mdray@ameritech.net>
2024-08-29 18:16:28 -0400

    fix: add vesper

---
+++ lua/17robots/lazy/color.lua
@@ -0,0 +1,16 @@
+return {
+  'datsfilipe/vesper.nvim',
+  config = function()
+    require'vesper'.setup({
+      transparent = true,
+      italics = {
+        comments = true,
+        keywords = true,
+        functions = true,
+        strings = true,
+        variables = true,
+      },
+    })
+    vim.cmd.colorscheme('vesper')
+  end
+}
