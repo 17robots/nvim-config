e2b79f73a22bd64e3fc859fcbe9eca3363b5c43f
17robots <mdray@duck.com>
2025-04-22 20:44:10 +0000

    fix: cleanup

--- lua/17robots/lazy/blink.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'saghen/blink.nvim',
-  build = 'cargo build --release',
-  keys = {
-    -- {'<leader>x', '<cmd>BlinkTree  toggle-focus<cr>' }
-  },
-  lazy = false,
-  opts = { tree = { enable = true }},
-}
