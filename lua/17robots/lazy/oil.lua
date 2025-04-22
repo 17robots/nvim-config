b098da4ea0d658ccc78cdec9cb519eac1a6ae638
17robots <mdray@duck.com>
2025-04-22 20:39:21 +0000

    fix: make oil float

--- lua/17robots/lazy/oil.lua
+++ lua/17robots/lazy/oil.lua
@@ -2,7 +2,7 @@ return {
   'stevearc/oil.nvim',
   opts = {},
   keys = {
-    {'<leader>x', '<cmd>Oil<cr>' }
+    {'<leader>x', '<cmd>Oil --float <cr>' }
   },
   dependencies = { 'echasnovski/mini.nvim' },
   lazy = false
