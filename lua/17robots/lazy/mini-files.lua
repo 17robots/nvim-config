671a55f3bb0c1a4e11a79c3711b3da9cb4d627fd
17robots <mdray@duck.com>
2025-09-18 19:05:15 -0400

    feat: things happening and updates

--- lua/17robots/lazy/mini-files.lua
+++ lua/17robots/lazy/mini-files.lua
@@ -1,7 +1,11 @@
 return {
   'echasnovski/mini.files',
   config = function()
-    require'mini.files'.setup()
+    require'mini.files'.setup{
+      options = {
+        use_as_default_explorer = true
+      }
+    }
   end,
   keys = {
     { '<leader>x', function() MiniFiles.open() end },
