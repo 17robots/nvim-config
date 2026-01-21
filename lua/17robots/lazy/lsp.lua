6c8464f7581e47a834f5e0ee3ab14e8162d43520
17robots <mdray@duck.com>
2026-01-21 15:39:51 -0500

    feat: snacks

--- lua/17robots/lazy/lsp.lua
+++ lua/17robots/lazy/lsp.lua
@@ -1,65 +1,17 @@
 return {
-  'neovim/nvim-lspconfig',
-  lazy = false,
+  'mason-org/mason-lspconfig.nvim',
   dependencies = {
-    { 'williamboman/mason.nvim', opts = {} },
-    'williamboman/mason-lspconfig.nvim',
-    'saghen/blink.cmp',
+    { 'mason-org/mason.nvim', opts = {} },
+    'neovim/nvim-lspconfig',
+    {
+      'saghen/blink.cmp',
+      opts = {
+        keymap = {
+          preset = 'default',
+          ['<CR>'] = { 'select_and_accept' }
+        }
+      },
+    },
   },
-  event = { "BufReadPost", "BufNewFile" },
-  config = function()
-    vim.api.nvim_create_autocmd('LspAttach', {
-      callback = function(event)
-        local opts = { buffer = event.buf }
-        vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, opts)
-        vim.keymap.set('n', '<leader>a', vim.lsp.buf.code_action, opts)
-        vim.keymap.set("n", "<leader>d", MiniExtra.pickers.diagnostic, opts)
-        vim.keymap.set("n", "gr", function() MiniExtra.pickers.lsp({ scope = 'references'}) end, opts)
-        vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-        vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "gd", function() MiniExtra.pickers.lsp({ scope = 'definition'}) end, opts)
-        vim.keymap.set("n", "<leader>i", MiniExtra.pickers.diagnostic, opts)
-
-        local function client_supports_method(client, method, bufnr)
-          if vim.fn.has 'nvim-0.11' == 1 then return client:supports_method(method, bufnr) else return client.supports_method(method) end
-        end
-        local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
-          vim.keymap.set('n', '<leader>th', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled{ bufnr = event.buf }) end)
-        end
-      end,
-    })
-    vim.diagnostic.config{
-      severity_sort = true,
-      float = { border = 'rounded', source = 'if_many' },
-      underline = { severity = vim.diagnostic.severity.ERROR },
-      virtual_text = {
-        source = 'if_many',
-        spacing = 2,
-        format = function(diagnostic)
-          local diagnostic_message = {
-            [vim.diagnostic.severity.ERROR] = diagnostic.message,
-            [vim.diagnostic.severity.WARN] = diagnostic.message,
-            [vim.diagnostic.severity.INFO] = diagnostic.message,
-            [vim.diagnostic.severity.HINT] = diagnostic.message,
-          }
-          return diagnostic_message[diagnostic.severity]
-        end,
-      },
-    }
-    local capabilities = require'blink.cmp'.get_lsp_capabilities()
-    local servers = {}
-    local ensure_installed = vim.tbl_keys(servers or {})
-    require'mason-lspconfig'.setup{
-      ensure_installed = ensure_installed,
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-          require'lspconfig'[server_name].setup(server)
-        end
-      },
-    }
-  end
+  opts = {},
 }
---
+++ lua/17robots/lazy/snacks.lua
@@ -0,0 +1,33 @@
+return {
+  'folke/snacks.nvim',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons'
+  },
+  priority = 1000,
+  lazy = false,
+  opts = {
+    bigfile = { enabled = true },
+    explorer = { enabled = true },
+    input = { enabled = true },
+    notifier = { enabled = true, timeout = 3000 },
+    picker = { enabled = true },
+    quickfile = { enabled = true },
+    scope = { enabled = true },
+    scroll = { enabled = true },
+    words = { enabled = true },
+  },
+  keys = {
+    { '<leader>f', function() Snacks.picker.smart() end, desc = 'Smart find files' },
+    { '<leader>b', function() Snacks.picker.buffers() end, desc = 'Buffers' },
+    { '<leader>/', function() Snacks.picker.grep() end, desc = 'Grep' },
+    { '<leader>n', function() Snacks.picker.notifications() end, desc = 'Notification History' },
+    { '<leader>x', function() Snacks.explorer() end, desc = 'File Explorer' },
+    { '<leader>d', function() Snacks.picker.diagnostics() end, desc = 'File Explorer' },
+    { 'gd', function() Snacks.picker.lsp_definitions() end, desc = 'Goto Definition' },
+    { 'gD', function() Snacks.picker.lsp_declarations() end, desc = 'Goto Declaration' },
+    { 'gr', function() Snacks.picker.lsp_references() end, desc = 'Goto References' },
+    { 'gI', function() Snacks.picker.lsp_implementations() end, desc = 'Goto Implementations' },
+    { '<leader>r', function() vim.lsp.buf.rename() end, desc = 'Rename Variable' },
+    { '<leader>a', function() vim.lsp.buf.code_action() end, desc = 'Rename Variable' },
+  }
+}
